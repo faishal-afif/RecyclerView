a5e15dfda6d13ccfaf3b719a0f65d2fdcce1b89d
Faishal Afif <afiffaishal@gmail.com>
2022-10-26 21:45:38 +0700

    Coba Commit dan Push

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -9,33 +9,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val stringNama: String = "Nama\t\t: "
+        val stringNim: String = "NIM\t\t\t: "
+        val stringHobi: String = "Hobi\t\t\t: "
         val memberList = listOf<Member>(
             Member(R.drawable.instagram,
-                "Ana Balqis Sholehah",
-                "205150200111063",
-                "abc"
+                "$stringNama Ana Balqis Sholehah",
+                "$stringNim 205150200111063",
+                "$stringHobi Ini Hobi Ana"
             ),
-            Member(R.drawable.instagram,
-                "Faishal Afif",
-                "205150201111041",
-                "abc"
+            Member(R.drawable.foto_afif,
+                "$stringNama Faishal Afif",
+                "$stringNim 205150201111041",
+                "$stringHobi Ini hobi Afif"
             ),
-            Member(R.drawable.instagram,
-                "Ika Febrianti",
-                "205150200111060",
-                "abc"
-            ),
-            Member(R.drawable.instagram,
-                "Lorem ipsum",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "abc"
-            ),
-            Member(R.drawable.instagram,
-                "Lorem ipsum",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "abc"
+            Member(R.drawable.foto_ika,
+                "$stringNama Ika Febrianti",
+                "$stringNim 205150200111060",
+                "$stringHobi Ini hobi Ika"
             )
+        // Nambah List secara manual
         )
 
         val recyclerView = findViewById<RecyclerView>(R.id.rv_member)
